97c8582bffe434ee272a36990d52d6e300757d22
Anandakrishnan <anandakrishnanayyappan@gmail.com>
2021-07-10 23:55:02 +0530

    added files

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const axios = require('axios')
 const _ = require('lodash')
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 let data = []
 
@@ -18,7 +20,7 @@ app.get('/', (req, res) => {
   axios(config)
     .then(function (response) {
       data = response.data
-      console.log(data)
+      // console.log(data)
       res.render('home', { content: data })
     })
     .catch(function (error) {
@@ -34,6 +36,30 @@ app.get('/continent/:cont', (req, res) => {
       res.render('continents', { continentData: data[i] })
     }
   }
+})
+let countrydata = []
+app.post('/', (req, res) => {
+  console.log(req.body.country)
+
+  var configcountry = {
+    method: 'get',
+    url: 'https://corona.lmao.ninja/v2/countries?yesterday&sort',
+    headers: {},
+  }
+
+  axios(configcountry)
+    .then(function (response) {
+      countrydata = response.data
+      console.log(countrydata)
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
+
+    for(let i=0;i<countrydata.length;i++){
+      if(countrydata[i].country===req.body.country)
+    }
+
 })
 
 app.listen(3000, (req, res) => {
